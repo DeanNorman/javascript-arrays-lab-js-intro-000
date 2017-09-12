bcd471fe3e89a407979e32f5695c49916b4a068e
Dean Norman <dean@22seven.com>
2017-09-12 08:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  return kittens.push(name);
+  return kittens.push(name); 
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
 }
